474c921d985374173c7ec35441083a1f6b749d6a
shm33z <mshimizu@bates.edu>
2024-05-24 06:43:02 -0400

    This is my first instagram tutorial

--- FinalProject_DCSs33/src/reducers.tsx
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { userSlice } from "./useSlice";
-
-export const rootReducer = combineReducers({
-  user: userSlice.reducer,
-});
--- FinalProject_DCSs33/src/store.tsx
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { rootReducer } from "./reducers.tsx";
-
-export const store = configureStore({
-  reducer: { data: rootReducer },
-});
